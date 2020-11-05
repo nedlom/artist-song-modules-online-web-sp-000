63dee25bd963e7803a73e3695513d476531ffe13
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 22:05:07 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,19 @@
 module Findable
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect {|a| a.name == name}
+  end
+  
 end
 
 
