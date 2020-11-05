121953c261a592878457eaa51528c62403b4aa17
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 22:19:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,36 +11,3 @@ class Song
   end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Song
-#   include Memorable::InstanceMethods, Paramable
-#   extend Memorable::ClassMethods, Findable
-  
-#   attr_accessor :name, :artist
-#   @@all = []
-  
-#   def self.all
-#     @@all
-#   end
-# end
