aeed10fa5190b9060ba613e3890d2ac15de93509
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 22:18:17 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -17,34 +17,3 @@ module Memorable
   end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# module Memorable
-#   module ClassMethods
-#     def reset_all
-#       self.all.clear
-#     end
-  
-#     def count
-#       self.all.count
-#     end
-#   end
-  
-#   module InstanceMethods
-#     def initialize
-#       self.class.all << self
-#     end
-#   end
-# end
